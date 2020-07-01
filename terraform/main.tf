3815ec2713a0315e4837d23e27116e88b078da60
AlexWhitham <55181116+AlexWhitham@users.noreply.github.com>
2020-07-01 09:55:37 +0100

    Passing name for the resource via prefix local variable

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,6 @@ data "aws_caller_identity" "current" {}
 
 locals  {
     prefix = "${var.app}-${var.stage}"
-    attachments_bucket = "${terraform.workspace}-${prefix}-attachments.${var.domain}"
-    emails_bucket = "${terraform.workspace}-${prefix}-emails.${var.domain}"
+    attachments_bucket = "${terraform.workspace}-${var.app}-${var.stage}-attachments.${var.domain}"
+    emails_bucket = "${terraform.workspace}-${var.app}-${var.stage}-emails.${var.domain}"
 }
