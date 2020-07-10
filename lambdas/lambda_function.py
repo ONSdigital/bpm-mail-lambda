a48c242bf0181836d0b456959e36823112fb44f4
Phil Bambridge <phil.bambridge@ons.gov.uk>
2020-07-10 12:14:22 +0100

    Refactor lambda to handle nifi delivery of emails

--- lambdas/lambda_function.py
+++ lambdas/lambda_function.py
@@ -1,51 +1,106 @@
 import json
-import requests
-import bleach
 from datetime import datetime
-from uuid import uuid4
-from email import policy, message_from_bytes
 from os import getenv
-import boto3
 from time import time
 import logging
-logger = logging.getLogger()
-logger.setLevel(logging.DEBUG)
+import boto3
+import jsonschema
+import requests
+import bleach
+
+LOGGER = logging.getLogger()
+LOGGER.setLevel(logging.DEBUG)
+
+MANIFEST_SCHEMA = {
+    "$schema": "http://json-schema.org/draft/2019-09/schema#",
+    "type": "object",
+    "properties": {
+        "subject": "string",
+        "sent": "string",
+        "ccList": "string",
+        "from": "string",
+        "emailmimetype": "string",
+        "Attachments": {
+            "type": "array",
+            "items": {
+                "type": "object",
+                "properties": {
+                    "filename": "string",
+                    "mailpart": "string",
+                    "orig.attach.name": "string",
+                    "attach.file.type": "string",
+                    "sizeBytes": "string",
+                    "body.mime.type": "string",
+                },
+            },
+        },
+    },
+}
+
+CSRF_TOKEN = None
+
+
+def check_token():
+    """Implements TTL-based local caching for BPM CSRF tokens"""
+    global CSRF_TOKEN
+    if CSRF_TOKEN is not None and CSRF_TOKEN.expiration < time():
+        return CSRF_TOKEN
+    csrf_resp = requests.post(
+        getenv("BPM_CSRF_URL"),
+        auth=(getenv("BPM_USER"), getenv("BPM_PW")),
+        json={"refresh_groups": True, "requested_lifetime": 7200},
+    )
+    if csrf_resp.status_code != 201:
+        raise Exception(
+            f"ERROR {csrf_resp.status_code}: Cannot get CSRF token: {csrf_resp.text}"
+        )
+    CSRF_TOKEN = csrf_resp.json()
+    # Subtracting 30 seconds to allow for bad clocks and latency
+    CSRF_TOKEN.expiration = (CSRF_TOKEN.expiration - 30) + int(time())
+    return CSRF_TOKEN
 
 
 def lambda_handler(event, context):
+    """Trigger a new Prices Correspondence process instance on new email.
+
+       Triggered by a new manifest file being delivered to S3 bucket.
+       Reads the manifest and email body files, and starts BPM Process
+       via REST API call.
+    """
+    global CSRF_TOKEN
     timestamp = time()
     client = boto3.client("s3")
     s3_event = event["Records"][0]["s3"]
-    
+
     bucket_name = s3_event["bucket"]["name"]
     s3_key = s3_event["object"]["key"]
-    logger.info(f"### S3 ### Bucket: {bucket_name}; Object key: {s3_key}")
+    LOGGER.info(f"### S3 ### Bucket: {bucket_name}; Object key: {s3_key}")
 
-    email_obj = client.get_object(
-        Bucket=bucket_name, Key=s3_key
-    )
-    logger.info(f"### TIME ### Time to get email: {time() - timestamp}")
+    manifest_txt = client.get_object(Bucket=bucket_name, Key=s3_key)
+    LOGGER.info(f"### TIME ### Time to get manifest: {time() - timestamp}")
     timestamp = time()
-    if email_obj == None:
+    if manifest_txt is None:
         raise Exception(
-            "Cannot access email from S3 bucket: " + s3_event["bucket"]["name"]
+            "Cannot access manifest from S3 bucket: " + s3_event["bucket"]["name"]
         )
 
-    message = message_from_bytes(email_obj["Body"].read(), policy=policy.default)
-    if message == None or len(message) == 0:
-        raise Exception(
-            "Email appears empty or unparseable: " + s3_event["object"]["key"]
-        )
-    logger.info(f"### TIME ### Time to parse email: {time() - timestamp}")
-    timestamp = time()
+    manifest = json.loads(manifest_txt)
+    try:
+        jsonschema.validate(instance=manifest, schema=MANIFEST_SCHEMA)
+    except jsonschema.exceptions.ValidationError:
+        raise Exception("Manifest fails validation: " + s3_event["object"]["key"])
+
     # We get:  Wed, 7 Oct 2015 12:34:56 -0700
     # We need: ISO-8601 format 'yyyy-MM-dd'T'HH:mm:ssz'
-    date_received = datetime.strptime(message.get("Date"), r"%a, %d %b %Y %H:%M:%S %z")
+    date_received = datetime.strptime(manifest.sent, r"%a, %d %b %Y %H:%M:%S %z")
     date_converted = date_received.strftime(r"%Y-%m-%dT%H:%M:%S%z")
-    email_body_part = message.get_body(preferencelist=("related", "plain", "html"))
-    logger.debug(email_body_part)
+
+    # Select the attachments object for the email body
+    body = [att for att in manifest.Attachments if att.mailpart == "body"][0]
+    # Retrieve email body from S3 bucket
+    email_body = client.get_object(Bucket=bucket_name, Key="email/" + body.filename)
     email_content = bleach.clean(
-        email_body_part.get_content(),
+        email_body,
         tags=[
             "a",
             "abbr",
@@ -65,71 +120,47 @@ def lambda_handler(event, context):
             "ul",
         ],
     )
-    logger.info(f"### TIME ### Time to sanitise html: {time() - timestamp}")
+    LOGGER.info(
+        f"### TIME ### Time to get email and sanitise html: {time() - timestamp}"
+    )
     timestamp = time()
-    attachments = []
-    bucket = getenv("ATTACHMENT_BUCKET")
-    
-    message_iter = message.iter_attachments()
-    attachment = next(message_iter, None)
-    if attachment != None:
+    attachments = [att for att in manifest.Attachments if att.mailpart == "attachment"]
+    req_attachments = []
+    if attachments:
+        bucket = getenv("ATTACHMENT_BUCKET")
         region = client.get_bucket_location(Bucket=bucket)["LocationConstraint"]
-        while attachment != None:
-            key = str(uuid4()) + str(uuid4())
-            content_type = attachment.get_content_type()
-            filename = attachment.get_filename()
-            file_content = attachment.get_content()
-            client.put_object(
-                Body=file_content,
-                Bucket=bucket,
-                Key=key,
-                ContentType=content_type,
-                ContentDisposition=f'attachment; filename="{filename}"',
-            )
-            attachments.append(
+        for attachment in attachments:
+            req_attachments.append(
                 {
-                    "url": f"https://s3.{region}.amazonaws.com/{bucket}/{key}",
-                    "contentType": content_type,
-                    "originalFilename": filename,
-                    "fileSizeBytes": len(file_content),
+                    "url": f"https://s3.{region}.amazonaws.com/{bucket}/{attachment.filename}",
+                    "contentType": attachment.get("attach.file.type"),
+                    "originalFilename": attachment.get("orig.attach.name"),
+                    "fileSizeBytes": int(attachment.sizeBytes),
                 }
             )
-            attachment = next(message_iter, None)
-    logger.info(f"### TIME ### Time to store attachments: {time() - timestamp}")
-    timestamp = time()
-    logger.info(f'### EMAIL ### From: {message.get("From")}, Subject: {message.get("Subject")}')
+
+    LOGGER.info(f'### EMAIL ### From: {manifest["from"]}, Subject: {manifest.subject}')
     bpm_data = {
         "input": [
             {
                 "name": "email",
                 "data": {
-                    "from": message.get("From"),
-                    "cc": message.get("Cc"),
+                    "from": manifest["from"],
+                    "cc": manifest.ccList,
                     "dateReceived": date_converted,
-                    "subject": message.get("Subject"),
+                    "subject": manifest.subject,
                     "body": email_content,
-                    "contentType": email_body_part.get_content_type(),
-                    "attachments": attachments,
+                    "contentType": body.get("body.mime.type"),
+                    "attachments": req_attachments,
                 },
             }
         ]
     }
 
-    csrf_resp = requests.post(
-        getenv("BPM_CSRF_URL"),
-        auth=(getenv("BPM_USER"), getenv("BPM_PW")),
-        json={"refresh_groups": True, "requested_lifetime": 7200},
-    )
-    if csrf_resp.status_code != 201:
-        raise Exception(
-            f"ERROR {csrf_resp.status_code}: Cannot get CSRF token: {csrf_resp.text}"
-        )
-    csrf_token = csrf_resp.json()["csrf_token"]
-    logger.info(f"### TIME ### Time to get csrf: {time() - timestamp}")
-    timestamp = time()
+    CSRF_TOKEN = check_token()
     task_resp = requests.post(
         getenv("BPM_EMAIL_URL"),
-        headers={"BPMCSRFToken": csrf_token},
+        headers={"BPMCSRFToken": CSRF_TOKEN.csrf_token},
         auth=(getenv("BPM_USER"), getenv("BPM_PW")),
         json=bpm_data,
     )
@@ -138,7 +169,7 @@ def lambda_handler(event, context):
         raise Exception(
             f"ERROR {task_resp.status_code}: Cannot init new task: {task_resp.text}"
         )
-    logger.info(f"### TIME ### Time to trigger bpm: {time() - timestamp}")
+    LOGGER.info(f"### TIME ### Time to trigger bpm: {time() - timestamp}")
     timestamp = time()
     return {
         "status": task_resp.status_code,
