5eaeed1123ba27fc29a861991c521ab09652d75a
Phil Bambridge <phil.bambridge@ons.gov.uk>
2020-10-27 22:24:31 +0000

    Quiet down the zipping; Add deletion permissions to deploy role

--- ci/tasks/bpm-mail-lambda.sh
+++ ci/tasks/bpm-mail-lambda.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 apt-get update
 apt-get -y install zip
-​
+
 mkdir generated
-​
+
 pushd bpm-mail-lambda
 python3 -m venv v-env
 source v-env/bin/activate
@@ -11,12 +11,12 @@ pip install pipenv
 pipenv install
 deactivate
 popd
-​
+
 mkdir zipit
-​
+
 mv bpm-mail-lambda/v-env/lib/python3.7/site-packages/* zipit/
 mv bpm-mail-lambda/lambdas/lambda_function.py zipit/
-​
+
 pushd zipit
-zip -r9 ../generated/function.zip .
+zip -qr9 ../generated/function.zip .
 popd
